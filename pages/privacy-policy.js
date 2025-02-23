20b1b47b24139f80a28dc6e32fa0e33c94730390
Cart <81428538+Cartrigger@users.noreply.github.com>
2025-02-23 01:31:04 -0500

    Update privacy-policy.js

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -26,18 +26,18 @@ export default function Home() {
             <div className="flex justify-center mt-20"></div>
             <div className="container px-6 py-10 mx-auto max-w-6xl -indent-5">
                 This privacy policy will reference of the following terms: QuestCraft (the "App"), QCXR (the "Developer"), the end-user interacting with the App (the "User"), information about your machine that is running the App (the "Device") and finally, third party account services not owned or operated by the Developer ("MSA", "Microsoft Authentication", "MCLogs", "Meta").
-<div><div>
+<br><br>
 1.0: The App requires a certain baseline set of information about the User and their Device to function for its intended purposes. The developer may be sent some information explained in 1.0a and 1.0b, and external developers in 1.1 and 1.2.
-<div><div>
+<br><br>
 1.0a: The App automatically may collect a limited amount of info about the User's Device, including (but not limited to) the Device model, operating system version, current time and date, and crashes. 
-<div><div>
+<br><br>
 1.0b: The App may not collect this data directly, and may collect this information from Meta. To see other information that is not shared with the developer, and sent to Meta or other device companies, please read their policies.
-<div><div>
+<br><br>
 1.1: The "Need Help" function of the App will send some of the collected info about the User game activity, and IP address to an external non-developer owned website known as "MCLogs" or "mclo.gs. Once this data is sent to the "Need Help" external service, the Developer and App have no authority over how this data is handled. The User should read the privacy policy of the MCLogs ("mclo.gs") service. Finally, the User ultimately has control over if the data is sent, and should exercise reasonable caution should they choose to send the data.
-<div><div>
+<br><br>
 1.2: Microsoft may collect some non-identifying information about your device, including (but not limited to) crash reports, installs per geographic region, or what type of device you are using. Please see Microsoft's Authentication and account privacy policies for more details.
 Some of this Microsoft-collected information may be accessible to QCXR. 
-<div><div>
+<br><br>
 Privacy policy updated on February 22nd, 2025.
             </div>
         </body>
